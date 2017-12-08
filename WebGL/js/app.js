d5f87c6b74e994e6d5a68dba7beb5652c6b8723a
Thomas Blanc <thomas.hakunamacouta.blanc@gmail.com>
2017-12-08 02:45:11 +0100

    Prout

--- WebGL/js/app.js
+++ WebGL/js/app.js
@@ -1,25 +1,91 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var scene, camera, renderer, geometry, material, cube, light;
+var stopLight, sky, sunSphere, tree1, tree2, tree3, controls, building, building2;
 
-var renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setClearColor( 0xffffff, 0);
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00AA11 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setClearColor( 0xffffff, 0);
 
-camera.position.z = 5;
+    light = new THREE.AmbientLight( 0xcccccc ); // soft white light
+    scene.add( light );
 
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    controls = new THREE.OrbitControls( camera );
+    camera.position.set( 0, 20, 50 );
+    controls.update();
 
-function listener(elem, evnt, func)
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+
+    initBuilding();
+    initStopLight();
+    initSky();
+}
+
+
+function initSky() {
+    scene.background = new THREE.CubeTextureLoader()
+        .setPath( 'img/' )
+        .load( [
+            'pr_bk.png',
+            'pr_lf.png',
+            'pr_up.png',
+            'pr_dn.png',
+            'pr_ft.png',
+            'pr_rt.png'
+        ] );
+    scene.background.minFilter = THREE.LinearFilter;
+}
+
+function initStopLight() {
+    stopLight = new THREE.MTLLoader();
+    stopLight.load( 'models/stop_Light.mtl', function( materials ) {
+        materials.preload();
+        var objLoader = new THREE.OBJLoader();
+        objLoader.setMaterials( materials );
+        objLoader.load( 'models/stop_Light.obj', function ( object ) {
+            object.position.y = -95;
+            object.rotation.y = 20;
+            scene.add( object );
+        });
+    });
+}
+
+function initBuilding() {
+    building = new THREE.MTLLoader();
+    building.load( 'models/building_2/materials.mtl', function( materials ) {
+        materials.preload();
+        var objLoader = new THREE.OBJLoader();
+        objLoader.setMaterials( materials );
+        objLoader.load( 'models/building_2/model.obj', function ( object ) {
+            object.scale.set(100,100,100);
+            object.position.x = 100;
+            scene.add( object );
+        });
+    });
+
+    building2 = new THREE.MTLLoader();
+    building2.load( 'models/building_1/materials.mtl', function( materials ) {
+        materials.preload();
+        var obj = new THREE.OBJLoader();
+        obj.setMaterials( materials );
+        obj.load( 'models/building_1/model.obj', function ( object ) {
+            object.scale.set(100,100,100);
+            object.position.x = -100;
+            scene.add( object );
+        });
+    });
+}
+
+function listener(elem, event, func)
 {
     if (elem.addEventListener)
-        elem.addEventListener(evnt,func,false);
+        elem.addEventListener(event,func,false);
     else if (elem.attachEvent) // For IE
-        return elem.attachEvent("on" + evnt, func);
+        return elem.attachEvent("on" + event, func);
 }
 
 function ev_keydown(e)
@@ -44,10 +110,15 @@ function ev_keydown(e)
 	}
 }
 
+function animate() {
+    requestAnimationFrame( animate );
+
+    controls.update();
+
+    renderer.render( scene, camera );
+}
+
 listener(document.body, 'keydown', ev_keydown);
 
-function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-}
+init();
 animate();
