33417c171b8fe1207371e4177f4ce73036cc8a1b
Florent Vain <florent.vain@etu.u-bordeaux.fr>
2017-12-08 02:39:52 +0100

    fock

--- WebGL/js/app.js
+++ WebGL/js/app.js
@@ -1,18 +1,117 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var scene, camera, renderer;
+var cssScene, cssCamera, cssRenderer;
+var sky, sunSphere;
 
-var renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setClearColor( 0xffffff, 0);
+var cameraForward = new THREE.Vector3(0,0,+1);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00AA11 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
 
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+function initGL()
+{
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	renderer = new THREE.WebGLRenderer({ alpha: true });
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setClearColor( 0xffffff, 0);
+
+	camera.position.z = 5;
+
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	var cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function initCSS3D()
+{
+	cssScene = new THREE.Scene();
+	cssCamera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	cssRenderer = new THREE.CSS3DRenderer();
+
+	var element = document.getElementById("test");
+	
+	var cssObject = new THREE.CSS3DObject( element );
+	cssObject.position.set(-1000,0,0);
+	cssObject.rotation.y = 90;
+	cssScene.add(cssObject);
+
+	element = document.getElementById("calendar");
+	cssObject = new THREE.CSS3DObject( element );
+	cssObject.position.set(1000,0,0);
+	cssObject.rotation.y = -90;
+	cssScene.add(cssObject);
+
+	cssRenderer.setSize( window.innerWidth, window.innerHeight );
+	cssRenderer.domElement.style.position = 'absolute';
+	cssRenderer.domElement.style.top = 0;
+	
+	document.body.appendChild(cssRenderer.domElement);	
+}
+
+function initAll()
+{
+	initGL();
+	initCSS3D();
+}
+
+var cssSpeedMult = 75;
+var MIN_Z =  30;
+var MAX_Z = -50;
+
+
+/** KeyDown Callback */
+function ev_keydown(e)
+{
+	var key = e.keyCode ? e.keyCode : e.which;
+
+	console.log(camera.position);
+	
+	/** UP ARROW */
+	if(key === 38)
+	{
+		if(camera.position.z < MAX_Z)
+			return;
+		camera.position.x -= cameraForward.x * 1 ;
+		camera.position.z -= cameraForward.z * 1 ;
+
+		cssCamera.position.x -= cameraForward.x * cssSpeedMult ;
+		cssCamera.position.z -= cameraForward.z * cssSpeedMult ;
+		
+	}
+	/** DOWN ARROW */
+	else if (key === 40)
+	{
+		if(camera.position.z > MIN_Z)
+			return;
+
+		camera.position.x += cameraForward.x * 1 ;
+		camera.position.z += cameraForward.z * 1 ;
+
+		cssCamera.position.x += cameraForward.x * cssSpeedMult ;
+		cssCamera.position.z += cameraForward.z * cssSpeedMult ;
+	}
+	/** RIGHT ARROW */
+	else if (key === 39)
+	{
+		camera.rotation.set(0, camera.rotation.y - Math.PI/2,0);
+		cssCamera.rotation.set(0, camera.rotation.y - Math.PI/2,0);
+
+		console.log(cameraForward);
+
+	}
+	/** LEFT ARROW */
+	else if (key === 37)
+	{
+		camera.rotation.set(0, camera.rotation.y + Math.PI/2,0);
+		cssCamera.rotation.set(0, camera.rotation.y + Math.PI/2,0);
+		
+		console.log(cameraForward);
+	}
+}
 
 function listener(elem, evnt, func)
 {
@@ -22,32 +121,15 @@ function listener(elem, evnt, func)
         return elem.attachEvent("on" + evnt, func);
 }
 
-function ev_keydown(e)
-{
-	var key = e.keyCode ? e.keyCode : e.which;
-		
-	if(key === 38)
-	{
-		camera.position.z += 1 ;
-	}
-	else if (key === 40)
-	{
-		camera.position.z -= 1;
-	}
-	else if (key === 39)
-	{
-		camera.rotation.y += 0.1;
-	}
-	else if (key === 37)
-	{
-		camera.rotation.y -= 0.1;
-	}
-}
-
 listener(document.body, 'keydown', ev_keydown);
 
+initAll();
+
 function animate() {
-	requestAnimationFrame( animate );
+	requestAnimationFrame( animate ); 
+
+	cssRenderer.render( cssScene, cssCamera);
 	renderer.render( scene, camera );
 }
+
 animate();
